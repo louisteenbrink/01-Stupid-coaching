3064dddac2f7bf5b71069484b49ce8237a64bb6e
louis steenbrink <louis.steenbrink@gmail.com>
2016-05-09 10:48:27 +0200

    add coaching controller

--- stupid_coaching/config/routes.rb
+++ stupid_coaching/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'coaching_controller/answer'
+
+  get 'coaching_controller/ask'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
