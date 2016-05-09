f6f44ab80424a9379a77fbdcc49d81bfc32ed38a
louis steenbrink <louis.steenbrink@gmail.com>
2016-05-09 12:09:00 +0200

    stupid coaching exercise done

--- stupid_coaching/app/controllers/coaching_controller.rb
+++ stupid_coaching/app/controllers/coaching_controller.rb
@@ -1,7 +1,25 @@
 class CoachingController < ApplicationController
+
   def answer
+    @question_asked = params[:question_asked]
+
   end
 
   def ask
   end
+
+
+  # def coach_answer_enhanced(your_message)
+  #   answer = coach_answer(your_message)
+  #   if answer != ""
+  #     if your_message.upcase == your_message
+  #       "I can feel your motivation! #{answer}"
+  #     else
+  #       answer
+  #     end
+  #   else
+  #     ""
+  #   end
+  # end
+
 end
--- stupid_coaching/config/routes.rb
+++ stupid_coaching/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'answer' => 'coaching_controller#answer'
+  get 'answer' => 'coaching#answer'
 
-  get 'coaching_controller/ask'
+  get 'ask' => 'coaching#ask'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
